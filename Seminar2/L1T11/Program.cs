6251ff4f143c812db3bdc1bf6726ad6b35330a3b
unknown <irina_prj@mail.ru>
2022-11-04 18:54:04 +0300

    тоже на отлично семинар2

---
+++ Seminar2/L1T11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 11 HARD - необязательная: Напишите программу, которая принимает на вход целое число любой разрядности число и удаляет вторую цифру слева направо этого
+// числа. И, конечно же, через строку решать нельзя.
+
+Console.Write("Число: ");
+int v_int = Convert.ToInt32(Console.ReadLine());
+int v_digits = (int)Math.Log10(v_int);
+Console.WriteLine("Digits:"+v_digits);
+int v_res = v_int/((int)Math.Pow(10,v_digits))*((int)Math.Pow(10,v_digits-1)) +
+            v_int%((int)Math.Pow(10,v_digits-1));
+Console.WriteLine(v_res);
