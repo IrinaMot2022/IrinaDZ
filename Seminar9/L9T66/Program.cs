4169c1501127d78203e892a509c064bb6adfe63b
unknown <irina_prj@mail.ru>
2022-11-16 17:46:48 +0300

    наконец-то все позади....

---
+++ Seminar9/L9T66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int fSum(int m, int n){
+    if(m==n) return n;
+    return m+fSum(m+1,n);
+}
+
+Console.WriteLine(fSum(1,15));
+Console.WriteLine(fSum(4,8));
---
+++ Seminar9/L9T68/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int f(int n, int m)
+{
+    if (n == 0) return m + 1; else
+      if ((n != 0) && (m == 0)) return f(n - 1, 1); else return f(n - 1, f(n, m - 1));
+}
+
+Console.WriteLine(f(2, 3));
+Console.WriteLine(f(3, 2));
